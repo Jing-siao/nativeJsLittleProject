384072a1a8be6bb006fbfe0e4d3ca5be6c64ef6c
Jing-siao <vicky820715@gmail.com>
2022-02-25 15:57:16 +0800

    日歷待修

--- js/datePicker.js
+++ js/datePicker.js
@@ -1,6 +1,9 @@
 
 var temp = true;
 var firstDay = -1;
+var days = 0;
+var rows = 0;
+
 //是否選擇中狀態
 function select() {
   temp = !temp;
@@ -34,10 +37,11 @@ function notId(e, text) {
 function getDays(select) {
   select.addEventListener("change", function () {
     if (yearSelect.value && monthSelect.value) {
-      let days = new Date(yearSelect.value, monthSelect.value, 0).getDate();
+      days = new Date(yearSelect.value, monthSelect.value, 0).getDate();
       daySelect.options.length = 1;
       createOption(1, days, daySelect);
       firstDay = new Date(yearSelect.value, monthSelect.value - 1, 1).getDay();
+      rows = Math.ceil((firstDay + days) / 7);
       console.log(firstDay);
     }
   });
@@ -48,9 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const daySelect = document.getElementById('daySelect');
   const dateChoose = document.getElementById('dateChoose');
   const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const table = document.createElement('table');
-  const tr = document.createElement('tr');
-  const calendar = document.getElementById("calendar");
+
   //select部分
   dateChoose.innerText = "請選擇日期";
   createOption(2010, 2025, yearSelect);
@@ -77,34 +79,86 @@ document.addEventListener("DOMContentLoaded", function () {
         select();
   });
   //日曆部份
-  // document.addEventListener('click', function () {
-  //   if (daySelect.value) {
-  //     calendar.appendChild(table);
-  //     table.appendChild(tr);
-  //     for (const i of week) {
-  //       let th = document.createElement('th');
-  //       th.innerText = i;
-  //       tr.appendChild(th);
-  //     }
-  //   }
-  // })
-  calendar.appendChild(table);
-  table.appendChild(tr);
-  let newTable = document.getElementsByTagName('table');
-  const td = document.createElement('td');
-  for (const i of week) {
-    let th = document.createElement('th');
-    th.innerText = i;
-    tr.appendChild(th);
-  }
-  // for (let i = 0; i < 5; i++) {
-  newTable.appendChild(tr);
-  // for (let j = 0; j < 7; j++) {
-  //   td.innerText = j
-  //   tr.appendChild(td);
+  daySelect.addEventListener('change', function () {
+    const calendar = document.getElementById("calendar");
+    const tableNode = document.createElement('table');
+    tableNode.id = 'tableId';
+    let tableElement = document.getElementsByTagName('table').length;
+    if (tableElement > 0) {
+      document.getElementById('tableId').remove();
+      let k = 0;
+      for (var i = 0; i < rows; i++) {
+        var trNode = tableNode.insertRow();
+        if (i === 0) {
+          for (const k of week) {
+            let th = document.createElement('th');
+            th.innerText = k;
+            trNode.appendChild(th);
+          }
+        } else {
+          // for (let y = 1; y <= 7; y++) {
+          //   let tdNode = trNode.insertCell();
+          //   let date = ((i - 1) * 7) + y;
+          //   date > days ?
+          //     tdNode.innerHTML = '' :
+          //     tdNode.innerHTML = date;
+          // }
+          for (let y = 0; y < 7; y++) {
+            let tdNode = trNode.insertCell();
+            k++;
+            if (k < firstDay || k > days) {
+              tdNode.innerHTML = ''
+            } else {
+              tdNode.innerHTML = k;
+            }
 
-  // }
-  // }
+          }
+        }
+      }
+      calendar.appendChild(tableNode);
+    } else {
+      let k = 0;
+      for (var i = 0; i < rows + 1; i++) {
+        var trNode = tableNode.insertRow();
+        if (i === 0) {
+          for (const k of week) {
+            let th = document.createElement('th');
+            th.innerText = k;
+            trNode.appendChild(th);
+          }
+        } else {
+          // for (let y = 1; y <= 7; y++) {
+          //   let tdNode = trNode.insertCell();
+          //   let date = ((i - 1) * 7) + y;
+          //   date > days ?
+          //     tdNode.innerHTML = '' :
+          //     tdNode.innerHTML = date;
+          //   // if (i < firstDay) {
+          //   //   tdNode.innerHTML = ''
+          //   // } else {
+          //   //   tdNode.innerHTML = date;
+          //   // }
+          // }
+          for (let y = 0; y < 7; y++) {
+            let tdNode = trNode.insertCell();
+            k++;
+            if (k < firstDay || k > days) {
+              tdNode.innerHTML = ''
+            } else {
+              tdNode.innerHTML = k;
+            }
+
+          }
+        }
+      }
+      calendar.appendChild(tableNode);
+    }
+
+
+
+  });
+
+
 });
 
 
